b17ce3e707f888115150a66e57c5fcd6f8daeb60
rigueira <rigueiravitorr@gmail.com>
2024-08-20 20:35:45 -0300

    instalado do banco de dados

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+// Configuração do Sequelize
+const db = require('./config/database');
+
+// Testar a conexão com o banco de dados
+db.authenticate()
+  .then(() => console.log('Conexão com o banco de dados estabelecida com sucesso.'))
+  .catch(err => console.error('Erro ao conectar com o banco de dados:', err));
+
+// Middlewares
+app.use(express.json());
+
+// Importar rotas
+// const produtosRoutes = require('./routes/produtosRoutes');
+
+// Usar rotas
+// app.use('/api', produtosRoutes);
+
+// Iniciar o servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+``
